a6476e2253e3ca750067d79116fce13d3a6b5177
Cheng Tan <noinil@gmail.com>
2014-05-27 10:43:25 +0800

    pdb_read functions

--- include/PDB.h
+++ include/PDB.h
@@ -3,15 +3,44 @@
 #ifndef PINANG_PDB_READ_H
 #define PINANG_PDB_READ_H
 
-#include "vec3d.h"
-#include "atom.h"
-#include "residue.h"
+#include "model.h"
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 namespace pinang{
 
+    class PDB
+    {
+    public:
+        PDB(const std::string& s);
+        virtual ~PDB() {_models.clear();}
+
+    protected:
+        std::string _PDB_file_name;
+        std::vector<Model> _models;
+        int _n_model;
+    };
+
+    PDB::PDB(const std::string& s)
+    {
+        Atom atom_tmp;
+        Residue resid_tmp;
+        Chain chain_tmp;
+        unsigned int tmp_ri = 0;
+
+        ifstream ifile(s.c_str());
+        if (!ifile.is_open())
+        {
+            std::cerr << "ERROR: Cannot read file: " << infilename << std::endl;
+            std::cerr << "Program terminating." << endl;
+            exit(EXIT_FAILURE);
+        }
+        while (ifile.good()) {
+            ifile >> atom_tmp;
+        }
+    }
 }
 
 #endif
