ab59a7323cac141574645a895e6f1578d52390e6
Cheng Tan <noinil@gmail.com>
2016-05-12 11:06:42 +0900

    improve vec3d.h

--- include/vec3d.h
+++ include/vec3d.h
@@ -14,66 +14,89 @@ namespace pinang{
 class Vec3d
 {
  public:
-  Vec3d(): _z1(0), _z2(0), _z3(0) {}
-  Vec3d(double a, double b, double c): _z1(a), _z2(b), _z3(c) {}
+  Vec3d(): z1_(0), z2_(0), z3_(0) {}
+  Vec3d(double a, double b, double c): z1_(a), z2_(b), z3_(c) {}
 
-  double x() const {return _z1;}
-  double y() const {return _z2;}
-  double z() const {return _z3;}
+  inline double x() const {return z1_;}
+  inline double y() const {return z2_;}
+  inline double z() const {return z3_;}
 
-  Vec3d operator+(const Vec3d& other) const
-  {
-    return Vec3d(_z1+other._z1,_z2+other._z2,_z3+other._z3);
-  }
+  inline Vec3d operator+(const Vec3d&) const;
+  inline Vec3d operator-(const Vec3d&) const;
+  inline Vec3d operator*(double) const;
+  inline double operator*(const Vec3d&) const;
+  inline Vec3d operator^(const Vec3d&) const;
+  inline double operator[](unsigned int) const;
 
-  Vec3d operator-(const Vec3d& other) const
-  {
-    return Vec3d(_z1-other._z1,_z2-other._z2,_z3-other._z3);
-  }
+  inline double norm() const;
+  inline double sqr_norm() const;
 
-  Vec3d operator*(const double x) const
-  {
-    return Vec3d(_z1*x,_z2*x,_z3*x);
-  }
-
-  double operator*(const Vec3d& other) const
-  {
-    return (_z1*other._z1+_z2*other._z2+_z3*other._z3);
-  }
-
-  Vec3d operator^(const Vec3d& other) const
-  {
-    return Vec3d(_z2*other._z3 - _z3*other._z2,
-                 _z3*other._z1 - _z1*other._z3,
-                 _z1*other._z2 - _z2*other._z1);
-  }
-
-  double operator[](unsigned int i) const
-  {
-    switch(i)
-    {
-      case 0: return _z1;
-      case 1: return _z2;
-      case 2: return _z3;
-      default:
-        std::cerr << "ERROR: 3D Vector index out of range!" << std::endl;
-        exit(EXIT_SUCCESS);
-    }
-  }
-
-  inline double norm() const
-  {
-    return sqrt(_z1*_z1+_z2*_z2+_z3*_z3);
-  }
-
-  inline double sqr_norm() const
-  {
-    return (_z1*_z1+_z2*_z2+_z3*_z3);
-  }
+  friend inline Vec3d operator*(double, const Vec3d&);
+  friend inline std::ostream& operator<<(std::ostream&, const Vec3d&);
+  friend inline std::istream& operator>>(std::istream&, Vec3d&);
+  friend inline double vec_distance(Vec3d&, Vec3d&);
+  friend inline double vec_angle(Vec3d&, Vec3d&);
+  friend inline double vec_angle_deg(Vec3d&, Vec3d&);
 
  protected:
-  double _z1, _z2, _z3;
+  double z1_, z2_, z3_;
 };
+// ------------------------------ MEMBERS --------------------
+inline Vec3d Vec3d::operator+(const Vec3d& other) const
+{
+  return Vec3d(z1_ + other.z1_, z2_ + other.z2_, z3_ + other.z3_);
+}
+
+inline Vec3d Vec3d::operator-(const Vec3d& other) const
+{
+  return Vec3d(z1_ - other.z1_, z2_ - other.z2_, z3_ - other.z3_);
+}
+
+inline Vec3d Vec3d::operator*(double x) const
+{
+  return Vec3d(z1_ * x, z2_ * x, z3_ * x);
+}
+
+inline double Vec3d::operator*(const Vec3d& other) const
+{
+  return (z1_ * other.z1_ + z2_ * other.z2_ + z3_ * other.z3_);
+}
+
+inline Vec3d Vec3d::operator^(const Vec3d& other) const
+{
+  return Vec3d(z2_ * other.z3_ - z3_ * other.z2_,
+               z3_ * other.z1_ - z1_ * other.z3_,
+               z1_ * other.z2_ - z2_ * other.z1_);
+}
+
+inline double Vec3d::operator[](unsigned int i) const
+{
+  switch(i)
+  {
+    case 0: return z1_;
+    case 1: return z2_;
+    case 2: return z3_;
+    default:
+      std::cerr << "ERROR: 3D Vector index out of range!" << std::endl;
+      exit(EXIT_SUCCESS);
+  }
+}
+
+inline double Vec3d::norm() const
+{
+  return sqrt(z1_ * z1_ + z2_ * z2_ + z3_ * z3_);
+}
+
+inline double Vec3d::sqr_norm() const
+{
+  return (z1_ * z1_ + z2_ * z2_ + z3_ * z3_);
+}
+
+// ------------------------------ FRIENDS --------------------
+inline Vec3d operator*(double x, const Vec3d & v)
+{
+  return Vec3d(v.z1_ * x, v.z2_ * x, v.z3_ * x);
+}
 
 inline std::ostream& operator<<(std::ostream& o, const Vec3d& v)
 {
@@ -96,27 +119,34 @@ inline std::istream& operator>>(std::istream& i, Vec3d& v)
   return i;
 }
 
-inline double vec_distance (Vec3d& v1, Vec3d& v2)
+inline double vec_distance(Vec3d& v1, Vec3d& v2)
 {
   Vec3d v3 = v1-v2;
   return v3.norm();
 }
 
-double vec_angle (Vec3d& v1, Vec3d& v2)
+inline double vec_angle(Vec3d& v1, Vec3d& v2)
 {
-  if ((v1*v2)/(v1.norm()*v2.norm()) >= 0.99999)
-    return 0;
-  if ((v1*v2)/(v1.norm()*v2.norm()) <= -0.99999)
+  double d = v1 * v2;
+  double m = v1.norm() * v2.norm();
+  double f = d / m;
+  if (f >= 0.99999)
+    return 0.0;
+  if (f <= -0.99999)
     return k_pi;
-  return acos((v1*v2)/(v1.norm()*v2.norm()));
+  return acos(f);
 }
-double vec_angle_deg (Vec3d& v1, Vec3d& v2)
+
+inline double vec_angle_deg(Vec3d& v1, Vec3d& v2)
 {
-  if ((v1*v2)/(v1.norm()*v2.norm()) >= 0.99999)
-    return 0;
-  if ((v1*v2)/(v1.norm()*v2.norm()) <= -0.99999)
-    return 180;
-  double ang = acos((v1*v2)/(v1.norm()*v2.norm()));
+  double d = v1 * v2;
+  double m = v1.norm() * v2.norm();
+  double f = d / m;
+  if (f >= 0.99999)
+    return 0.0;
+  if (f <= -0.99999)
+    return 180.0;
+  double ang = acos(f);
   return (180 * ang / k_pi);
 }
 
