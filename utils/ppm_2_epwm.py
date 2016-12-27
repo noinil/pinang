36a2a6ddf972deb95d46cc7657f7f2d6efaac0ee
Cheng Tan <noinil@gmail.com>
2016-12-27 15:53:10 +0900

    ppm and pfm to pwm.

--- utils/ppm_2_epwm.py
+++ utils/ppm_2_epwm.py
@@ -3,30 +3,28 @@
 import sys
 import numpy as np
 
-def main(pfmname):
-    pfm = {}
-    with open(pfmname, 'r') as pfm_fin:
-        for line in pfm_fin:
+def main(ppmname):
+    ppm = {}
+    with open(ppmname, 'r') as ppm_fin:
+        for line in ppm_fin:
             words = line.split()
             if len(words) < 1:
                 continue
             if words[0] in ['A:', 'C:', 'G:', 'T:']:
                 local_p = []
-                for pfm_value in words[1:]:
-                    v = float(pfm_value)
+                for ppm_value in words[1:]:
+                    v = float(ppm_value)
                     local_p.append(v)
-                pfm[words[0][0]] = local_p[:]
+                ppm[words[0][0]] = local_p[:]
 
     en_from_p = {}
     en_from_p['A'] = []
     en_from_p['C'] = []
     en_from_p['G'] = []
     en_from_p['T'] = []
-    len_DNA = len(pfm['A'])
+    len_DNA = len(ppm['A'])
     for i in range(len_DNA):
-        fA, fC, fG, fT = pfm['A'][i], pfm['C'][i], pfm['G'][i], pfm['T'][i]
-        f_sum = fA + fC + fG + fT
-        pA, pC, pG, pT = fA / f_sum, fC / f_sum, fG / f_sum, fT / f_sum
+        pA, pC, pG, pT = ppm['A'][i], ppm['C'][i], ppm['G'][i], ppm['T'][i]
         eA, eC, eG, eT = -np.log(pA/0.25), -np.log(pC/0.25), -np.log(pG/0.25), -np.log(pT/0.25)
         e_ave = (eA + eC + eG + eT) / 4
         eA -= e_ave
@@ -40,7 +38,7 @@ def main(pfmname):
 
     # Output
     outstr = " {0:8.4f} "
-    outfilename = pfmname[:-3] + "pwm"
+    outfilename = ppmname[:-3] + "pwm"
     outfile = open(outfilename, 'w')
     for bname in ['A', 'C', 'G', 'T']:
         outfile.write(bname + ": ")
@@ -51,5 +49,5 @@ def main(pfmname):
 
 
 if __name__ == '__main__':
-    pfm_fname = sys.argv[1]
-    main(pfm_fname)
+    ppm_fname = sys.argv[1]
+    main(ppm_fname)
